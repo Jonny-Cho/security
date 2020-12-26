8ee5621ecbe69664fc09fb0828c69c88a5b6ff61
Jonny-Cho <andante2183@gmail.com>
2020-12-26 14:38:51 +0900

    add remember-me

--- src/main/kotlin/com/security/SecurityConfig.kt
+++ src/main/kotlin/com/security/SecurityConfig.kt
@@ -4,25 +4,34 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.userdetails.UserDetailsService
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig : WebSecurityConfigurerAdapter() {
+class SecurityConfig(val userDetailsService:UserDetailsService) : WebSecurityConfigurerAdapter() {
 
 	@Override
 	override fun configure(http: HttpSecurity) {
-		http.authorizeRequests()
+		http
+			.authorizeRequests()
 			.anyRequest().authenticated()
 
 		http
 			.formLogin()
 //			.loginPage("/loginPage")
-			.defaultSuccessUrl("/success", true)
+			.defaultSuccessUrl("/success")
 			.failureUrl("/fail")
 			.usernameParameter("user")
 			.passwordParameter("pw")
 			.loginProcessingUrl("/login_proc")
 			.permitAll()
+
+		http
+			.rememberMe()
+			.rememberMeParameter("remember")
+			.tokenValiditySeconds(3600)
+//			.alwaysRemember(true)
+			.userDetailsService(userDetailsService)
 	}
 
 }
