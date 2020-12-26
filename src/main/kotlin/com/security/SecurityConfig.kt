e6a37581b6387642d3bcb833128e88e2206f045a
Jonny-Cho <andante2183@gmail.com>
2020-12-26 18:29:41 +0900

    add antMatchers

--- src/main/kotlin/com/security/SecurityConfig.kt
+++ src/main/kotlin/com/security/SecurityConfig.kt
@@ -1,20 +1,30 @@
 package com.security
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig(val userDetailsService: UserDetailsService) : WebSecurityConfigurerAdapter() {
 
+	override fun configure(auth: AuthenticationManagerBuilder) {
+		auth.inMemoryAuthentication().withUser("user").password("{noop}1234").roles("USER")
+		auth.inMemoryAuthentication().withUser("sys").password("{noop}1234").roles("SYS","USER")
+		auth.inMemoryAuthentication().withUser("admin").password("{noop}1234").roles("ADMIN","USER")
+	}
+
 	@Override
 	override fun configure(http: HttpSecurity) {
 		http.authorizeRequests()
-			.anyRequest()
-			.authenticated()
+			.antMatchers("/user").hasRole("USER")
+			.antMatchers("/admin/pay").hasRole("ADMIN")
+			.antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
+			.anyRequest().authenticated()
 
 		http.formLogin()
 			//			.loginPage("/loginPage")
@@ -36,9 +46,8 @@ class SecurityConfig(val userDetailsService: UserDetailsService) : WebSecurityCo
 			.maxSessionsPreventsLogin(false) // 동시 로그인 차단 default: false -> 기존 세션 만료
 			.expiredUrl("/expired")
 			.and()
-			.invalidSessionUrl("/invalid")
-			.sessionFixation()
-			.changeSessionId()
+			.sessionFixation().changeSessionId()
+			.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED) // 기본값 IF_REQUIRED. JWT등을 사용할때는 Stateless로 설정
 	}
 
 }
--- src/main/kotlin/com/security/controller/CtlMain.kt
+++ src/main/kotlin/com/security/controller/CtlMain.kt
@@ -1,15 +1,20 @@
 package com.security.controller
 
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 class CtlMain {
 
 	@GetMapping("/")
-	@ResponseBody
-	fun main():String {
-		return "hello world!"
-	}
+	fun main() = "hello world!"
+
+	@GetMapping("/user")
+	fun user() = "user"
+
+	@GetMapping("/admin/pay")
+	fun adminPay() = "adminPay"
+
+	@GetMapping("/admin/**")
+	fun admin() = "admin"
 }
