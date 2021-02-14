c8d4d28dc0f2cb213d8360208e65619c4605843d
Jonny-Cho <andante2183@gmail.com>
2021-02-15 00:18:08 +0900

    bug fix

--- src/main/kotlin/com/security/controller/user/MessageController.kt
+++ src/main/kotlin/com/security/controller/user/MessageController.kt
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
@@ -11,7 +12,7 @@ class MessageController {
 	@GetMapping("/messages")
 	fun message() = "user/messages"
 
-	@GetMapping("/api/messages")
+	@PostMapping("/api/messages")
 	@ResponseBody
 	fun apiMessage():ResponseEntity<String> {
 		return ResponseEntity("ok", HttpStatus.OK)
